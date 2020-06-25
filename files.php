f43a96096aa1032d880fdcb3dc5dd980b1744caf
swr8bit <swr8bit@gmail.com>
2020-06-25 22:04:54 +0300

    updated log.php add links to files.php

--- files.php
+++ files.php
@@ -13,23 +13,22 @@
 			include	"header.php";
 		?>
 	
-    <div class="contentWrap">
+    
             <div class="content">
                 <div class="center">
-	
-	                <h1>Файлы</h1>
-
-		            <div class="box">
-                        <p><a href="https://github.com/swr8bit/site">Репозиторий этого сайта на GitHub</a></p>
-                        <p></p>
-                                                    
-                           
-                             <a href="log.php">Лог</a>
                     
-                    </div>
+	                    <h1>Ссылки и файлы</h1>
+		                <div class="box_files">
+                            <h2></h2>
+                           <a href="https://github.com/swr8bit/site"><b>Репозиторий этого сайта на GitHub</a>
+                            <p></p>
+                                                    
+                             <a href="/files/huinya.zip"><b>Скачать архивчик моих учебных заданий</a>
+                        </div>    
+                    
                 </div>
 		    </div>
-    </div>
+    
 
 </div>
 <div class="footer">
