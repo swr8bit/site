7c371ccacfca3cd8c484fcf54f5638af9d41d0ae
swr8bit <swr8bit@gmail.com>
2020-07-01 20:49:48 +0300

    add pop-ups

--- mycerts.php
+++ mycerts.php
@@ -1,5 +1,5 @@
 <p><b>Мои сертификаты об окончании курсов GeekBrains:</b>
 <br><br>
-<a href="https://geekbrains.ru/certificates/892320">- "Основы программирования"</a>
+<a href="https://geekbrains.ru/certificates/892320" target="popup" onclick="window.open('https://geekbrains.ru/certificates/892320','popup','width=600,height=600'); return false;">- "Основы программирования"</a>
 <br><br>
-<a href="https://geekbrains.ru/certificates/872389">- "Git. Базовый курс"</a></p>
+<a href="https://geekbrains.ru/certificates/872389" target="popup" onclick="window.open('https://geekbrains.ru/certificates/872389','popup','width=600,height=600'); return false;">- "Git. Базовый курс"</a></p>
